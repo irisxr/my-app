eb57f1f7d63c981655535992b10593aeb81647e2
iris <1340531394@qq.com>
2019-10-23 23:56:17 +0800

    初识单文件组件

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <div id="app">
+    <input type="text" v-model="info">
+    <button @click="addToDoItem()">addItem</button>
+    <ul>
+      <todo-item v-for="item in list">
+        <template v-slot:item="itemProps">
+          <span :style="{fontSize: '30px',color:itemProps.checked ?'red':'blue'}">{{item}}</span>
+        </template>
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import todoItem from './components/todoItem.vue'
+export default {
+  name: 'app',
+    data(){
+      return{
+          info:'',
+          list:[],
+      }
+    },
+    methods:{
+        addToDoItem(){
+            this.list.push(this.info);
+            this.info=''
+        }
+    },
+
+  components: {
+    todoItem,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="itemStyle">
+<input type="checkbox" v-model="checked">
+        <slot name="item" v-bind="{checked}"></slot>
+    </li>
+</template>
+
+
+<script>
+    export default {
+        props:['item'],
+        data(){
+            return{
+                checked:false
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+.itemStyle{
+    color: #ffddaa;
+    list-style: none;
+}
+</style>
