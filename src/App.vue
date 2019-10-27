b0ffa46e72ac6bb4dfa8bab5e8d196445cbec7f5
iris <1340531394@qq.com>
2019-10-27 23:45:44 +0800

    属性

--- src/App.vue
+++ src/App.vue
@@ -9,29 +9,44 @@
         </template>
       </todo-item>
     </ul>
+
+    <props
+    name="Hello Iris"
+    :isVisible="true"
+    :type="type"
+    :on-change="handleChange"
+    >
+    </props>
   </div>
 </template>
 
 <script>
 
 import todoItem from './components/todoItem.vue'
+import props from './components/props.vue'
 export default {
   name: 'app',
     data(){
       return{
           info:'',
           list:[],
+          type:'success'
+
       }
     },
     methods:{
         addToDoItem(){
             this.list.push(this.info);
             this.info=''
+        },
+        handleChange(val){
+            this.type=val;
         }
     },
 
   components: {
     todoItem,
+      props,
   }
 }
 </script>
---
+++ src/components/props.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        name:{{name}}<br/>
+        isVisible:{{isVisible}}<br/>
+        list:{{list}}<br/>
+        type:{{type}}<br/>
+        <button @click="handleThing">change thing</button>
+
+    </div>
+</template>
+
+
+<style>
+
+</style>
+
+
+<script>
+    export default {
+        props: {
+            name: String,
+            isVisible: {
+                type: Boolean,
+                default: false,
+            },
+            list: {
+                type: Array,
+                default() {
+                    return []
+                },
+            },
+            type: {
+                validator: function (value) {
+                    return ['success', 'warning', 'danger'].includes(value)
+                }
+            },
+            onChange: {
+                type: Function,
+                default:()=>{}
+            },
+        },
+        methods:{
+            handleThing(){
+                //this.type='warning'
+                this.onChange(this.type==='success'?'warning':'success')
+            }
+        }
+    }
+</script>
